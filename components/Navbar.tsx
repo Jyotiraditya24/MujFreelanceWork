d24220cc5503fdbda580fad56eaa19264b258387
jyotiraditya <jyotiraditya24@gmail.com>
2025-02-11 22:58:23 +0530

    MUJ-WEBSITE

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const navItems = [
+  { label: "Home", href: "/home" },
+  { label: "Material", href: "/material" },
+  { label: "Enterprise", href: "/enterprise" },
+  { label: "Pricing", href: "/pricing" },
+  { label: "Docs", href: "/docs" },
+  { label: "Contact Sales", href: "/contact-sales" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-5 left-1/2 -translate-x-1/2 bg-white shadow-md py-2 px-10 flex justify-center gap-x-10 items-center rounded-2xl w-max z-50 border border-t border-black">
+      <div className="text-xl font-bold font-serif">MujTopper</div>
+      <div className="flex gap-x-6">
+        {navItems.map((item) => (
+          <Link
+            href={item.href}
+            key={item.label}
+            className="text-gray-700 hover:text-black font-serif"
+          >
+            {item.label}
+          </Link>
+        ))}
+      </div>
+      <button className="bg-yellow-200 text-black px-4 py-2 rounded-2xl">Sign in</button>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,33 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        blink: "cursor-blink 0.8s infinite",
+        words: "words-keyframes 6s steps(1) infinite",
+        typing:
+          "typing-keyframes 4s steps(14) infinite, cursor-blink 0.8s infinite",
+        "typing-words":
+          "typing-keyframes 4s steps(14) infinite, cursor-blink 0.8s infinite, words-keyframes 6s steps(1) infinite", // ✅ Combined animation
+      },
+      keyframes: {
+        "cursor-blink": {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
+        "words-keyframes": {
+          "0%, 20%": { content: '"Gym"' },
+          "21%, 40%": { content: '"Shark"' },
+          "41%, 60%": { content: '"Fitness"' },
+          "61%, 80%": { content: '"Power"' },
+          "81%, 100%": { content: '"Hello"' },
+        },
+        "typing-keyframes": {
+          "0%, 15%, 30%, 35%, 50%, 55%, 70%, 75%, 90%, 95%": { width: "0" },
+          "5%, 20%, 25%, 40%, 45%, 60%, 65%, 80%, 85%": {
+            width: "calc(100% + 8px)",
+          },
+        },
+      },
     },
   },
   plugins: [],
