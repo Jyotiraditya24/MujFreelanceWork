76dc8eefa217e9c349c7107b7fd928bf1c2399be
jyotiraditya <jyotiraditya24@gmail.com>
2025-02-21 02:10:31 +0530

    added a lot of links

--- app/branch/[yearBranch]/subjects/[branch]/page.tsx
+++ app/branch/[yearBranch]/subjects/[branch]/page.tsx
@@ -5,135 +5,326 @@ export default async function SubjectPage({
 }: {
   params: Promise<{ branch: string; yearBranch: string }>;
 }) {
-  // Await the params Promise to extract branch and yearBranch
   const { branch, yearBranch } = await params;
 
-  // Subjects grouped by branch and year
-  const subjectsYearWise: Record<string, Record<string, string[]>> = {
+  const subjectsYearWise: Record<
+    string,
+    Record<
+      string,
+      {
+        name: string;
+        image: string;
+        pyqLink: string;
+        notesLink: string;
+        playlistLink: string;
+      }[]
+    >
+  > = {
+    PhysicsCycle: {
+      first: [
+        {
+          name: "Engineering Materials & Mechanics",
+          image: "/EMM.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1w54A2Vd87DQtFwEoUuy8m0nX9rANb4RA?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/1nxBvjZVxG2QL4W_B8skkg4z-VYibmF7n?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "Biology For Engineers",
+          image: "/BFE.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1kNkblB1RDznXNJmehDohPTvMGckCHAfY?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/178MPsfJrZA6TSfvdAbdpcjww1KFAeXiL?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "Engineering Physics",
+          image: "/EP.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1cdiHPynlmRHmneEPs7P6EV1DB7wzQGsh?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/17txyc5ZmKvQepWdVK1TCnJQgxaoou6Ke?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "Computational Mathematics",
+          image: "/CM.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1wE3l2pM-frCwyFS8DaRIW13-XCbmnAmv?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/17PMTzl-T1TOhnpWUxlVwIkY7OGhDM7TO?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "MATLAB",
+          image: "/MATLAB.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1d75bxh_L7JvAKNheNZ3c6YOeqikO1CTB?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/1VVt2FfnbjhGTebrL7ditH--K_BfWMJZa?usp=drive_link",
+          playlistLink: "",
+        },
+      ],
+    },
+    ChemistryCycle: {
+      first: [
+        {
+          name: "Calculus & Matrices",
+          image: "/EMM.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1Pr_H4-fb_80Jo6Du96qIIpa6FIVEDTNm?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/1sC2cCnpEBGosunT4snNZf2Y8pUj5-uqV?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "Electrical & Electronics System",
+          image: "/EMM.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1T2KhOCchylff1UyLZ2jyipmGmHtDx1Sd?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/1JnVhkK5vg93hTzYq11Cr7NE8seneqdU1?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "Engineering Chemistry",
+          image: "/EMM.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1pP4zpZHNyD9Zp1peFPRYePpMXvwEWZCV?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/1hER7WzvapxKzG4i3pIayLRU3ICTYwjSF?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "Problem Solving using Computers",
+          image: "/EMM.webp",
+          pyqLink:
+            "https://drive.google.com/drive/folders/14E0SRP4X0EbccxY9jMo44E7_zlUEARJb?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/1nj16NcCCIgI3kmmkT-eisrKg_BKN2yKA?usp=drive_link",
+          playlistLink: "",
+        },
+        {
+          name: "Technical Writing Clinic",
+          image: "/images/technical_writing.jpg",
+          pyqLink:
+            "https://drive.google.com/drive/folders/1Ka7AT18HKHu_WjIBSoloGI_9VHm-ar5t?usp=drive_link",
+          notesLink:
+            "https://drive.google.com/drive/folders/1y-t8alIoH2J7w_f1vBWYTWKcYkpHAIDV?usp=drive_link",
+          playlistLink: "",
+        },
+      ],
+    },
     IT: {
-      second: ["Economics", "Data Com", "CSA", "OOP", "RDBMS"],
-      third: ["FDS", "SE", "DAA", "CN", "AI/ML", "ACD", "CIS"],
+      second: [
+        {
+          name: "Economics",
+          image: "/images/economics.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "Data Com",
+          image: "/images/data-com.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "CSA",
+          image: "/images/csa.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "OOP",
+          image: "/images/oop.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "RDBMS",
+          image: "/images/rdbms.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+      ],
+      third: [
+        {
+          name: "FDS",
+          image: "/images/fds.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "SE",
+          image: "/images/se.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "DAA",
+          image: "/images/daa.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "CN",
+          image: "/images/cn.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "AI/ML",
+          image: "/images/ai-ml.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "ACD",
+          image: "/images/acd.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "CIS",
+          image: "/images/cis.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+      ],
     },
     CSE: {
       second: [
-        "Economics",
-        "EM-3",
-        "Data Com",
-        "CSA",
-        "OOP",
-        "EM-4",
-        "OS",
-        "RDBMS",
-      ],
-      third: ["AISC", "DAA", "ACD", "CN", "SE", "ISS", "Data Science & ML"],
-    },
-    PhysicsCycle: {
-      first: ["EMM", "EP", "CM", "BFE", "MATLAB", "COI", "UHV", "EVS"],
-    },
-    ChemistryCycle: {
-      first: ["EES", "CAM", "EC", "PSUC", "CAI", "TWC"],
-    },
-    DSE: {
-      second: [
-        "MFDS-1",
-        "Finance & Econometrics",
-        "Introduction to data analytics",
-        "OOP",
-        "DSA",
-        "CSA",
-        "MFDS-2",
-        "RDBMS",
-        "ML",
-        "DAA",
-        "DCN",
-      ],
-      third: [
-        "MFDS-3",
-        "DL",
-        "OS",
-        "NLP",
-        "Cloud Computing",
-        "Operations Research",
-        "AI",
-        "Parallel Programming",
-        "Big Data Analytics",
-        "Data Privacy and Security",
-      ],
-    },
-    CCE: {
-      second: [
-        "Economics",
-        "EM-3",
-        "Data Com",
-        "DDCA",
-        "OOP",
-        "EM-4",
-        "OS",
-        "RDBMS",
-        "CN",
+        {
+          name: "Economics",
+          image: "/images/economics.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "EM-3",
+          image: "/images/em3.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "Data Com",
+          image: "/images/data-com.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "CSA",
+          image: "/images/csa.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "OOP",
+          image: "/images/oop.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
       ],
       third: [
-        "SE",
-        "DAA",
-        "FDS",
-        "CIS",
-        "AI/ML",
-        "Wireless Communication",
-        "ACD",
-      ],
-    },
-    IOT: {
-      second: [
-        "EM-3",
-        "DDCA",
-        "Data Com",
-        "DSA",
-        "OOP",
-        "Economics",
-        "EM-4",
-        "OS",
-        "RDBMS",
-        "Web Tech",
-        "ACD",
-      ],
-      third: [
-        "DAA",
-        "AI/ML",
-        "Sensors and Microcontroller",
-        "IOT Architecture & Its Protocols",
-        "SE",
-        "CIS",
-        "IOT Design",
-        "Deep Learning",
-        "CN",
-      ],
-    },
-    AIML: {
-      third: [
-        "Soft Computing Methods",
-        "DAA",
-        "CN",
-        "FDS",
-        "Game Theory",
-        "ML",
-        "CV",
-        "NLP",
-        "Data Mining",
-        "Fundamentals of Automobile Engineering",
+        {
+          name: "AISC",
+          image: "/images/aisc.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "DAA",
+          image: "/images/daa.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "ACD",
+          image: "/images/acd.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "CN",
+          image: "/images/cn.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "SE",
+          image: "/images/se.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "ISS",
+          image: "/images/iss.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
+        {
+          name: "Data Science & ML",
+          image: "/images/dsml.jpg",
+          pyqLink: "",
+          notesLink: "",
+          playlistLink: "",
+        },
       ],
     },
   };
-
-  // Get subjects based on branch and year
+  console.log(yearBranch + " " + branch);
   const subjects = subjectsYearWise[branch]?.[yearBranch];
 
-  // Reusable function to render subjects
-  const renderSubjects = (subjects: string[] | undefined) => {
+  const renderSubjects = (
+    subjects:
+      | {
+          name: string;
+          image: string;
+          pyqLink: string;
+          notesLink: string;
+          playlistLink: string;
+        }[]
+      | undefined
+  ) => {
     if (!subjects) return <div>No subjects found</div>;
     return (
       <div className="flex flex-row flex-wrap gap-y-6 mt-20 p-10 text-black justify-between">
-        {subjects.map((subject) => (
-          <CardWithSelect key={subject}></CardWithSelect>
+        {subjects.map(({ name, image, pyqLink, notesLink, playlistLink }) => (
+          <CardWithSelect
+            key={name}
+            subjectName={name}
+            subjectImage={image}
+            pyqLink={pyqLink}
+            notesLink={notesLink}
+            playlistLink={playlistLink}
+          />
         ))}
       </div>
     );
--- components/CardSelectComponeny.tsx
+++ components/CardSelectComponeny.tsx
@@ -6,55 +6,63 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 
-export default function CardWithSelect() {
+export default function CardWithSelect({
+  subjectName,
+  subjectImage,
+  pyqLink,
+  notesLink,
+  playlistLink,
+}: {
+  subjectName: string;
+  subjectImage: string;
+  pyqLink: string;
+  notesLink: string;
+  playlistLink: string;
+}) {
   const handleSelection = (value: string) => {
-    if (value === "apple") {
-      window.open(
-        "https://drive.google.com/file/d/1kGeEDFDuECsxvdYFg221sVJfbDcg9psb/view?usp=sharing"
-      );
-    }
+    const links: Record<string, string> = {
+      pyq: pyqLink,
+      notes: notesLink,
+      playlist: playlistLink,
+    };
+    if (links[value]) window.open(links[value], "_blank");
   };
 
   return (
-    <div className="w-72 bg-white shadow-xl rounded-2xl p-5 flex flex-col items-center border border-gray-200">
-      {/* Title Section */}
-      <h2 className="text-xl font-bold text-gray-800 text-center">
-        Physics Cycle
-      </h2>
-      <div className="w-16 border-b-2 border-gray-300 mt-1 mb-3"></div>
-
-      {/* Image Section */}
-      <div className="w-full h-40 relative overflow-hidden rounded-lg shadow-sm">
+    <div className="w-80 bg-white shadow-lg rounded-xl p-6 flex flex-col items-center border border-gray-300 transition-transform transform hover:scale-105 duration-200">
+      {/* Image Section with Overlay */}
+      <div className="w-full h-44 relative overflow-hidden rounded-xl shadow-md mb-4">
         <Image
-          src="/Physics_Cycle.png"
-          alt="Card Image"
+          src={subjectImage}
+          alt={subjectName}
           layout="fill"
           objectFit="cover"
-          className="rounded-lg"
+          className="rounded-xl"
         />
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 rounded-xl">
+          <h2 className="text-lg font-semibold text-white text-center">
+            {subjectName}
+          </h2>
+        </div>
       </div>
 
       {/* Select Option */}
-      <div className="mt-4 w-full">
-        <Select onValueChange={handleSelection}>
-          <SelectTrigger className="w-full border-gray-300 rounded-lg shadow-sm">
-            <SelectValue placeholder="Select an Option" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Resources</SelectLabel>
-              <SelectItem value="apple">PYQ</SelectItem>
-              <SelectItem value="banana">Toppers Note</SelectItem>
-              <SelectItem value="blueberry">PlayList</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
+      <Select onValueChange={handleSelection}>
+        <SelectTrigger className="w-full border-gray-300 rounded-lg bg-gray-50 shadow-sm hover:bg-gray-100 focus:ring-2 focus:ring-blue-500">
+          <SelectValue placeholder="📚 Select a Resource" />
+        </SelectTrigger>
+        <SelectContent className="bg-white border border-gray-200 rounded-lg shadow-md">
+          <SelectGroup>
+            <SelectItem value="pyq">📜 PYQ (Past Year Papers)</SelectItem>
+            <SelectItem value="notes">📖 Toppers' Notes</SelectItem>
+            <SelectItem value="playlist">🎥 Video Playlist</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
   );
 }
