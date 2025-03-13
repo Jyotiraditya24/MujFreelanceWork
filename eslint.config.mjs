8802a9271c317863534342ef0e6da89e37722244
jyotiraditya <jyotiraditya24@gmail.com>
2025-03-14 02:26:02 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,14 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      // Disable specific rules
       "import/no-anonymous-default-export": "off",
       "react/display-name": "off",
       "prefer-const": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "react-hooks/exhaustive-deps": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off", // Disables the error for unescaped entities
     },
   },
 ];
