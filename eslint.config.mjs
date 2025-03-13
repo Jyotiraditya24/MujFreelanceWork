903249d9f013ef0801aa28405f2e5f68cb7ac7f0
jyotiraditya <jyotiraditya24@gmail.com>
2025-03-13 20:59:38 +0530

    changed unused vars for prod

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ export default [
       "prefer-const": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
