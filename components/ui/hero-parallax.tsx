f3d5da0efde82edf4e55cb9c0ad273c1162fd6b8
jyotiraditya <jyotiraditya24@gmail.com>
2025-03-17 16:49:24 +0530

    github changes and a little mobile changes

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -57,7 +57,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[260vh] md:h-[320vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[270vh] md:h-[330vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
